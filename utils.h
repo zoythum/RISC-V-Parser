1057891ab939e3e05d29661b91ee20825854d730
Tomas Antonio Lopez <tomasantonio.lopez@mail.polimi.it>
2019-05-10 02:29:50 +0200

    Moved isTokenDelim() prototype to the header file for utility functions

--- utils.h
+++ utils.h
@@ -1,7 +1,15 @@
 #include "data.h"
+
+int isTokenDelim(char value);
+
 uint32_t Hash (const char * data, int len);
+
 family family_finder(char *work);
+
 reg register_finder(char *in);
+
 as_directive directive_finder(char *work);
+
 char *strip_front(char *input, int size);
+
 char *strip_back(char *input, int size);
